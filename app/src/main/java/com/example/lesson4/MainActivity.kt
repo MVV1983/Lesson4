6becd9555f1c3b2ab53d9cbc528e5c4f8ec1b2cc
v.matyukhin <silf083@gmail.com>
2021-05-05 20:16:59 +0700

    Верстка по макету

--- app/src/main/java/com/example/lesson4/MainActivity.kt
+++ app/src/main/java/com/example/lesson4/MainActivity.kt
@@ -1,9 +1,8 @@
 package com.example.lesson4
 
-import android.content.Context
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatDelegate
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
