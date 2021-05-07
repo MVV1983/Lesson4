29b188ae4d570662e0d1e6e292949eb9f24131d1
v.matyukhin <silf083@gmail.com>
2021-05-08 02:53:03 +0700

    Верстка по макету Добавил + в AppBar добавил arrow

--- app/src/main/java/com/example/lesson4/MainActivity.kt
+++ app/src/main/java/com/example/lesson4/MainActivity.kt
@@ -3,12 +3,15 @@ package com.example.lesson4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
@@ -22,5 +25,11 @@ class MainActivity : AppCompatActivity() {
 
         customAdapter.update(Data().data)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.info_menu,menu)
+        return true
+    }
+
 
 }
--- app/src/main/java/com/example/lesson4/StudentHolder.kt
+++ app/src/main/java/com/example/lesson4/StudentHolder.kt
@@ -1,34 +1,52 @@
 package com.example.lesson4
 
+import android.R.attr.button
 import android.content.Context
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.marginEnd
+import androidx.core.view.marginRight
 import androidx.recyclerview.widget.RecyclerView
 
-class StudentHolder(parent: ViewGroup, context: Context) : RecyclerView.ViewHolder(LayoutInflater.from(context).inflate(R.layout.student_item,parent,false)){
+
+class StudentHolder(parent: ViewGroup, context: Context) : RecyclerView.ViewHolder(
+    LayoutInflater.from(context).inflate(R.layout.student_item, parent, false)
+) {
+
 
     var studentFIO: TextView? = null
     var studentDescription: TextView? = null
     var image: ImageView
+    var imageArrow: ImageView
 
     init {
         studentFIO = itemView.findViewById(R.id.student_fi)
         studentDescription = itemView.findViewById(R.id.student_description)
+
         image = itemView.findViewById(R.id.imageView)
+        imageArrow = itemView.findViewById(R.id.arrowImage)
         image.setImageResource(R.drawable.ic_si_history)
+        imageArrow.setImageResource(R.drawable.ic_arrow)
     }
 
-    fun bind(item: ListItem){
+    fun bind(item: ListItem) {
         val index = item as ListItem.StudentItem
-        var fio = index.name+" "+index.secondName
+        var fio = index.secondName + " " + index.name
 
         studentFIO?.text = fio
         studentDescription?.text = index.description
-        if(index.hasPortfolio){
-            //visible element
 
+        if (index.hasPortfolio) {
+            //visible element
+            imageArrow.setVisibility(View.VISIBLE)
+
+        } else {
+            imageArrow.setVisibility(View.GONE)
         }
     }
 
