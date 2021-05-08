78f8961b5f4ca34ea959222b1e70a9ef4edc2829
v.matyukhin <silf083@gmail.com>
2021-05-08 13:21:33 +0700

    Верстка по макету Добавил + в AppBar добавил arrow

--- app/src/main/java/com/example/lesson4/BannerHolder.kt
+++ app/src/main/java/com/example/lesson4/BannerHolder.kt
@@ -7,23 +7,26 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class BannerHolder(parent: ViewGroup,context: Context) : RecyclerView.ViewHolder(LayoutInflater.from(context).inflate(R.layout.banner_item,parent,false)) {
-
+class BannerHolder(parent: ViewGroup, context: Context) : RecyclerView.ViewHolder(
+    LayoutInflater.from(context).inflate(R.layout.banner_item, parent, false)
+) {
 
 
     var newIncident: TextView? = null
     var discription: TextView? = null
     var imageBanner: ImageView
+    var accept: TextView? = null
 
     init {
         newIncident = itemView.findViewById(R.id.newIncident)
         discription = itemView.findViewById(R.id.descriptionIncident)
         imageBanner = itemView.findViewById(R.id.imageView2)
         imageBanner.setImageResource(R.drawable.ic_banner)
+        accept = itemView.findViewById(R.id.takeBtn)
 
     }
 
-    fun bind(item: ListItem){
+    fun bind(item: ListItem) {
 
         val index = item as ListItem.BannerItem
 
